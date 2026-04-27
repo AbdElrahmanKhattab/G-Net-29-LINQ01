b341a05c370ec069be5316aa59aed5ad23abeae8
Moataz-AMY <manaatef123@gmail.com>
2026-04-27 19:47:28 +0200

    Question 1

--- LINQ 01/Program.cs
+++ LINQ 01/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace LINQ_01
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQ_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Sample Data
+
+            List<Product> ProductList = new List<Product>
+            {
+                new Product{ ProductName="Chai", Category="Beverages", UnitPrice=18, UnitsInStock=39 },
+                new Product{ ProductName="Chang", Category="Beverages", UnitPrice=19, UnitsInStock=17 },
+                new Product{ ProductName="Aniseed Syrup", Category="Condiments", UnitPrice=10, UnitsInStock=13 },
+                new Product{ ProductName="Chef Anton", Category="Condiments", UnitPrice=22, UnitsInStock=0 },
+                new Product{ ProductName="Ikura", Category="Seafood", UnitPrice=31, UnitsInStock=20 },
+                new Product{ ProductName="Konbu", Category="Seafood", UnitPrice=6, UnitsInStock=0 },
+                new Product{ ProductName="Cajun Seasoning", Category="Condiments", UnitPrice=22, UnitsInStock=53 }
+            };
+
+            List<Order> Orders = new List<Order>
+            {
+                new Order{ CustomerID="ALFKI", OrderDate=new DateTime(1996,5,1)},
+                new Order{ CustomerID="BONAP", OrderDate=new DateTime(1998,7,10)},
+                new Order{ CustomerID="CACTU", OrderDate=new DateTime(1997,3,15)}
+            };
+
+            #endregion
+
+            #region 1 Seafood Products
+
+            var seafood = ProductList
+                .Where(p => p.Category == "Seafood");
+
+            foreach (var p in seafood)
+                Console.WriteLine($"{p.ProductName} - {p.UnitPrice}");
+
+            #endregion
         }
     }
 }
